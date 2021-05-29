195ad74948654622ae9953dff0c494d9b9b17e83
DominicSpears <dominicspears24@gmail.com>
2021-05-29 15:16:12 +0000

    add edit cuisines functionality

--- app.py
+++ app.py
@@ -191,6 +191,20 @@ def add_cuisine():
     return render_template("add_cuisine.html")
 
 
+@app.route("/edit_cuisine/<cuisine_id>", methods=["GET", "POST"])
+def edit_cuisine(cuisine_id):
+    if request.method == "POST":
+        submit = {
+            "cuisine_name": request.form.get("cuisine_name")
+        }
+        mongo.db.cuisines.update({"_id": ObjectId(cuisine_id)}, submit)
+        flash("Cuisine Successfully Updated")
+        return redirect(url_for("get_cuisines"))
+
+    cuisine = mongo.db.cuisines.find_one({"_id": ObjectId(cuisine_id)})
+    return render_template("edit_cuisine.html", cuisine=cuisine)
+
+
 # debug should = false when finalising project
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
